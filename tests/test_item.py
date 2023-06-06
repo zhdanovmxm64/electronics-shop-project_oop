ebc389849193f42b149f93f2af5d193cf3e22b78
zhdanovmxm64 <zhdanovmxm64@gmail.com>
2023-06-06 18:05:10 +0400

    test coverage 100%

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
+import pytest
+import pytest as pytest
+
+from src.item import Item
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+@pytest.fixture
+def item_fixture():
+    return Item("Смартфон", 10000, 20)
+
+def test_calculate_total_price(item_fixture):
+    assert item_fixture.calculate_total_price() == 200000
+
+def test_apply_discount(item_fixture):
+    Item.pay_rate = 0.8
+    item_fixture.apply_discount
+    assert item_fixture.price == 8000.0
